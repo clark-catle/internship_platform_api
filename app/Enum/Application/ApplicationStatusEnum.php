5c6a9de8b7bdd6b9772649ac70f3a026fbea06b3
Clark David <clarkcatle8@gmail.com>
2026-06-10 21:29:52 +0800

    added all the core controllers

--- app/Enum/Application/ApplicationStatusEnum.php
+++ app/Enum/Application/ApplicationStatusEnum.php
@@ -5,6 +5,7 @@ namespace App\Enum\Application;
 enum ApplicationStatusEnum: string
 {
     case Pending = 'pending';
+    case InReview = 'in review';
     case Accepted = 'accepted';
     case Rejected = 'rejected';
 }
